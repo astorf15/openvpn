1446fc007ffb2790534f9d207ee8cc5c80351070
astorf15 <astorf@inbox.ru>
2023-03-02 11:37:29 +0300

    YC-config renew

--- yandex-cloud/instances.tf
+++ yandex-cloud/instances.tf
@@ -1,36 +1,30 @@
-# yc compute instance
-resource "yandex_compute_instance" "k8s-m" {
-  count                     = 1
+resource "yandex_compute_instance" "node01" {
+  name                      = "vpn"
   zone                      = "ru-central1-a"
-  name                      = "vpn-${count.index + 1}"
-  hostname                  = "vpn-${count.index + 1}"
-  platform_id               = "standard-v1"
+  hostname                  = "vpn"
   allow_stopping_for_update = true
-  # lifecycle {
-  # ignore_changes = all
-  #}
+
   resources {
     cores  = 2
     memory = 2
   }
-
+  
   boot_disk {
     initialize_params {
-      image_id = var.ubuntu_22
-      name     = "vpn-${count.index + 1}"
-      type     = "network-ssd"
-      size     = "10"
+      image_id = var.ubuntu_20
+      name     = "disk-vpn"
+      type     = "network-nvme"
+      size     = "50"
     }
   }
 
   network_interface {
     subnet_id  = yandex_vpc_subnet.subnet.id
     nat        = true
-    ip_address = "10.2.2.1${count.index + 1}"
+    ip_address = "10.2.2.10"
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/fozzy.pub")}"
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
 }
-
